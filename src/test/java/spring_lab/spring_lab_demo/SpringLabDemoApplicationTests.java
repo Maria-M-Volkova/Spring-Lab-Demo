b4e163e1e5a2eec9b39ea13316a82b6882a831ae
Maria Volkova <maria.m.volkova@gmail.com>
2023-12-18 01:38:21 +0600

    first commit

---
+++ src/test/java/spring_lab/spring_lab_demo/SpringLabDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_lab.spring_lab_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLabDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
